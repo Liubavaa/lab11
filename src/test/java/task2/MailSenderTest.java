792d367c5ffa495e46cc5e31a53ff96ee0e67b84
Liubavaa <liubomyr.oleksiuk@ucu.edu.ua>
2022-12-26 02:16:06 +0200

    set 18 jdk

--- src/test/java/task2/MailSenderTest.java
+++ src/test/java/task2/MailSenderTest.java
@@ -1,12 +1,13 @@
 package task2;
 
 import com.mailjet.client.errors.MailjetException;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MailSenderTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void sendMail() throws MailjetException {
         GiftMail giftMail = new GiftMail();
         Client client = new Client("Liuba", 18, Gender.MALE, "liubomyr.oleksiuk@ucu.edu.ua");
